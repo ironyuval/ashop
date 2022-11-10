0ed6f24163ce7762e22183c13b194c3ed200d8ff
Gal Ben yosef <galbenyosef@users.noreply.github.com>
2022-11-10 17:15:19 +0200

    Update ProductRoute.js

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -2,13 +2,20 @@ import { Router } from 'express';
 import {
   getAllProducts, createProduct, updateProduct, deleteProduct, getSingleProduct,
 } from '../controller/ProductController';
+import Auth from '../auth';
 
 const router = Router();
 
+// Read - normal user
 router.route('/:id').get(getSingleProduct);
 router.route('/').get(getAllProducts);
+// Write - admin user
 router.route('/new').post(createProduct);
-router.route(':id').put(updateProduct);
+router.route('/:id').put(updateProduct);
 router.route('/:id').delete(deleteProduct);
+
+/* router.post('/new', Auth, createProduct);
+router.put('/:id', Auth, updateProduct);
+router.delete('/:id', Auth, deleteProduct); */
 
 export default router;
