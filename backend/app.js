0d134b132848b31a66420361f4a65fb0c9d8df2a
Gal Ben yosef <galbenyosef@users.noreply.github.com>
2022-11-11 16:20:18 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,10 +17,10 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, './frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './frontend/build/index.html'));
+  res.sendFile(path.resolve(__dirname, 'frontend/build/index.html'));
 });
 
 export default app;
