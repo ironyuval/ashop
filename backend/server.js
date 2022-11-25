635e5ae97a6ea513b051339327bcb3ae89b7dcf6
Gal Ben yosef <galbenyosef@users.noreply.github.com>
2022-11-25 19:21:14 +0200

    aa

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
-import { app } from './app.js';
-import { connectDb } from './db/connectDb';
+import { app } from './app';
+import connectDb from './db/connectDb';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 dotenv.config(isDevelopment ? {
