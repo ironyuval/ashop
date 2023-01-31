6b9d3652e7f387d847dc017f4313ae19c3cb9e6e
Gal Ben yosef <galbenyosef@users.noreply.github.com>
2023-01-31 22:10:24 +0200

    aa

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,18 +1,65 @@
+import "./About.css";
+
 function About(props) {
   return (
     <div className="container about m-auto">
       <h1>About This Project</h1>
-      <p>Hello and Welcome to Comix</p>
       <p>An online comic eshop, created as MERN Stack development project.</p>
-      <br />
-      <p>Using Create React App as frontend ui library</p>
-      <p>Using ReduxJS Toolkit as react global state management</p>
-      <br />
-      <h3>Browse items on destinated server database collection.</h3>
       <p>
-        Using NodeJS, ExpressJS and MongoDB Cloud as Rest API and database
-        server
+        use our quick and advanced searches to Browse over our products api
+        database
       </p>
+      <h3>User Roles</h3>
+      <table>
+        <thead>
+          <td>Role</td>
+          <td>Watch</td>
+          <td>Wishlist</td>
+          <td>Cart</td>
+          <td>Update Profile</td>
+          <td>C.r.u.d Product</td>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Non-Registered</td>
+            <td>&#9989;</td>
+            <td>&#9940;</td>
+            <td>&#9940;</td>
+            <td>&#9940;</td>
+            <td>&#9940;</td>
+          </tr>
+          <tr>
+            <td>Registered</td>
+            <td>&#9989;</td>
+            <td>&#9989;</td>
+            <td>&#9989;</td>
+            <td>&#9989;</td>
+            <td>&#9940;</td>
+          </tr>
+          <tr>
+            <td>Admin</td>
+            <td>&#9989;</td>
+            <td>&#9989;</td>
+            <td>&#9989;</td>
+            <td>&#9989;</td>
+            <td>&#9989;</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <br />
+      <h3>Frontend</h3>
+      <p>Create React App as frontend ui library</p>
+      <p>Bootstrap as styling library</p>
+      <p>ReduxJS Toolkit as react global state management</p>
+      <p>Axios as api service</p>
+      <p>Joi as validation package</p>
+      <br />
+      <h3>Backend</h3>
+      <p>NodeJs as server environment</p>
+      <p>ExpressJS as REST API framework</p>
+      <p>MongoDB Cloud as database server</p>
+      <p>mongoose as JS MongoDB driver</p>
     </div>
   );
 }
