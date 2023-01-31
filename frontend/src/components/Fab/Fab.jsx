eda79c419134e9e566ea9c8aad8c0e69200fab6b
Gal Ben yosef <galbenyosef@users.noreply.github.com>
2023-01-31 22:14:10 +0200

    aa

--- frontend/src/components/Fab/Fab.jsx
+++ frontend/src/components/Fab/Fab.jsx
@@ -1,6 +1,11 @@
+import { Roles } from "../../server-shared/types";
+import { useSelector } from "react-redux";
 import "./Fab.css";
 
 const Fab = ({ onClick = () => {} }) => {
+  const user = useSelector((state) => state.core.user);
+
+  if (!user || user.role === Roles.User) return null;
   return (
     <div onClick={onClick} className="fab-container">
       <div className="button iconbutton">
